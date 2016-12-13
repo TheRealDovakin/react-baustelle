1ff5e9248e48295fb3ec55fbb749b863e3845791
Kasper Nadrajkowski <nadrajkowski@live.de>
2016-12-13 12:10:53 +0100

    NewProcessPage, added link to k&b-phonebook to every Item

---
+++ src/js/pages/NewProcessPage.jss
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default class NewProcessPage extends React.Component{
+	render(){
+		return(
+			<h1>New Process Page</h1>
+		);
+	}
+}
---
+++ src/js/pages/ProcessListPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ProcessList from "../components/ProcessList"
+
+export default class ProcessListPage extends React.Component{
+	render(){
+		return(
+			<ProcessList/>
+		);
+	}
+}
