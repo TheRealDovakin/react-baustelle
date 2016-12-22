58276ca03f72ae925d3a48e04a7688a776fc30f2
Kasper Nadrajkowski <nadrajkowski@live.de>
2016-12-22 11:29:00 +0100

    formatted date in process

--- src/js/components/Process.js
+++ src/js/components/Process.js
@@ -10,13 +10,19 @@ export default class Process extends React.Component{
 
 		const { _id, status, person_name, due_date, p_type } = this.props;
 
+		var date = new Date(due_date);
+		var day = date.getDate();
+		var month = date.getMonth()+1;
+		var year = date.getFullYear();
+		var formatted_date = day+"."+month+"."+year;
+
 		if(status==1){//nicht erledigt
 			return(
 				<div class="panel panel-default">
 					<div class="panel-heading"><a href={"#/processView/"+_id}>{person_name}</a></div>
 					<ul class="list-group">
 						<a class="list-group-item">Status: in progress</a>
-						<a class="list-group-item">Deadline: {due_date}</a>
+						<a class="list-group-item">Deadline: {formatted_date}</a>
 						<a class="list-group-item">Typ: {p_type}</a>
 					</ul>	
 				</div>
