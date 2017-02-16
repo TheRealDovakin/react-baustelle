8bf45c0dc73977619c63bb2cdeced93c0aae55bb
Kasper Nadrajkowski <nadrajkowski@live.de>
2017-02-16 10:13:29 +0100

    adjusted app to dns name

--- kup-client/src/js/values/constants.js
+++ kup-client/src/js/values/constants.js
@@ -4,9 +4,9 @@
  */
 module.exports = {
   //links
-  appPath: 'https://172.22.23.6',
+  appPath: 'https://epm.kiebackpeter.kup',
   githubPath: 'http://www.github.com/therealdovakin/react-baustelle/',
   myMail: 'nadrajkowski@live.de',
   phoneBookLink: "http://edvweb.kiebackpeter.kup/telefon/index_html?sortorder=name&start:int=0&res_name=%25",
-  restApiPath: 'https://172.22.23.6/api/rest/',
+  restApiPath: 'https://epm.kiebackpeter.kup/api/rest/',
 }
